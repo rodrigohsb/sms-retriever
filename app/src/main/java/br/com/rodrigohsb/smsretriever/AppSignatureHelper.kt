a937c78ae12bb69206e396757c9e76b2452ba026
Rodrigo <rodrigo.bacellar@stone.com.br>
2018-03-15 17:52:09 -0300

    remove String format

--- app/src/main/java/br/com/rodrigohsb/smsretriever/AppSignatureHelper.kt
+++ app/src/main/java/br/com/rodrigohsb/smsretriever/AppSignatureHelper.kt
@@ -35,7 +35,7 @@ class AppSignatureHelper(context: Context): ContextWrapper(context) {
             // For each signature create a compatible hash
             signatures
                     .mapNotNull { hash(packageName, it.toCharsString()) }
-                    .mapTo(appCodes) { String.format("%s", it) }
+                    .mapTo(appCodes) { it }
             return appCodes
         } catch (e: PackageManager.NameNotFoundException) {
             Log.e(TAG, "Unable to find package to obtain hash.", e)
@@ -56,7 +56,7 @@ class AppSignatureHelper(context: Context): ContextWrapper(context) {
             var base64Hash = Base64.encodeToString(hashSignature, Base64.NO_PADDING or Base64.NO_WRAP)
             base64Hash = base64Hash.substring(0, NUM_BASE64_CHAR)
 
-            Log.d(TAG, String.format("pkg: %s -- hash: %s", packageName, base64Hash))
+            Log.d(TAG, "pkg: $packageName -- hash: $base64Hash")
             base64Hash
         } catch (e: NoSuchAlgorithmException) {
             Log.e(TAG, "hash:NoSuchAlgorithm", e)
